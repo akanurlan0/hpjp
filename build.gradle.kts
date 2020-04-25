48291934b5302d3687a9e31dea360c379b95e6c9
Nurlan Akashayev <nurlan.akashayev@btsdigital.kz>
2020-04-25 16:20:00 +0600

    p6spy

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("io.github.microutils:kotlin-logging:${property("kotlinLoggingVersion")}")
+	implementation("p6spy:p6spy:3.8.3")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
